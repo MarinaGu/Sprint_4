44e346a94ec6eea66b4b73e1e3de749e264ebe05
Marina Guseva <kravtzovamarina@yandex.ru>
2023-03-03 18:44:56 +0300

    correct tests and add parametrize

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 from pages.ya_page import MainPage
 from pages.form_order_page import FormOrderPage
 from pages.start_page import StartPage
@@ -6,23 +7,25 @@ from pages.start_page import StartPage
 
 @allure.story("tests")
 @allure.feature('test order')
-def test_order_up_button(browser):
+@pytest.mark.parametrize('name, sername, adress, phone, data, comment', [['Иван', 'Иванов', 'Москва', '891134567824', '23.03.2023', 'утром'], ['Петр', 'Петров', 'Москва, Сокольники', '89124568932', '31.03.2023', ' ']])
+
+def test_order_up_button(browser, name, sername, adress, phone, data, comment):
     start_page = StartPage(browser)
     form_order = FormOrderPage(browser)
 
     start_page.go_to_site()
     start_page.click_cookie_button()
     start_page.click_order_up_button()
-    form_order.enter_name("Имя")
-    form_order.enter_sername("Фамилия")
-    form_order.enter_adress("Адрес")
+    form_order.enter_name(name)
+    form_order.enter_sername(sername)
+    form_order.enter_adress(adress)
     form_order.enter_metro()
-    form_order.enter_phone("89113456789")
+    form_order.enter_phone(phone)
     form_order.click_next_button()
-    form_order.enter_data("21.03.2023")
+    form_order.enter_data(data)
     form_order.enter_period()
     form_order.enter_color()
-    form_order.enter_comment("")
+    form_order.enter_comment(comment)
     form_order.click_form_rent_order_button()
     form_order.click_order_ok_button()
     order_text = form_order.check_text_order()
@@ -32,7 +35,8 @@ def test_order_up_button(browser):
     check_text = start_page.check_start_page()
     assert "Самокат" in check_text
 
-def test_order_down_button(browser):
+@pytest.mark.parametrize('name, sername, adress, phone, data, comment', [['Аркадий', 'Захаров', 'Красная площадь', '89234567894', '28.04.2023', 'днем'], ['Дмитрий', 'круглов', 'Москва', '89654321785', '18.03.2023', ' ']])
+def test_order_down_button(browser, name, sername, adress, phone, data, comment):
     start_page = StartPage(browser)
     form_order = FormOrderPage(browser)
     ya_page = MainPage(browser)
@@ -41,16 +45,16 @@ def test_order_down_button(browser):
     start_page.click_cookie_button()
     start_page.click_order_down_button()
 
-    form_order.enter_name("Петя")
-    form_order.enter_sername("Петров")
-    form_order.enter_adress("Москва")
+    form_order.enter_name(name)
+    form_order.enter_sername(sername)
+    form_order.enter_adress(adress)
     form_order.enter_metro()
-    form_order.enter_phone("89113452345")
+    form_order.enter_phone(phone)
     form_order.click_next_button()
-    form_order.enter_data("26.04.2023")
+    form_order.enter_data(data)
     form_order.enter_period()
     form_order.enter_color()
-    form_order.enter_comment("")
+    form_order.enter_comment(comment)
     form_order.click_form_rent_order_button()
     form_order.click_order_ok_button()
     order_text = form_order.check_text_order()
